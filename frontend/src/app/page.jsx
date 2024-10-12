c343a2aefcb325337806c345bd35ffec2744e175
Joshua Zhang <zhy0224@gmail.com>
2024-10-12 14:00:25 -0700

    Move back

---
+++ frontend/src/app/page.jsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+        <div>
+            <div>
+                <h1>Welcome Back!</h1>
+            </div>
+            <div>
+                <input type="text" placeholder="Search Song"/>
+            </div>
+        </div>
+    </div>
+  );
+}
