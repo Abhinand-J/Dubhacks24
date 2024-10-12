b64e118f0ed4ef0e1109b344a0d1569ba5eee920
Abhinand <wabhinandj@gmail.com>
2024-10-12 12:39:00 -0700

    added videos_data version 1

---
+++ videos_data.py
@@ -0,0 +1,25 @@
+# videos_data.py
+
+videos = [
+    {
+        'id': 1,
+        'title': 'Python for Beginners',
+        'description': 'An introductory tutorial to Python.',
+        'url': 'https://example.com/python-for-beginners',
+        'duration': '10:23',
+    },
+    {
+        'id': 2,
+        'title': 'Advanced Python',
+        'description': 'A deep dive into Python features.',
+        'url': 'https://example.com/advanced-python',
+        'duration': '20:45',
+    },
+    {
+        'id': 3,
+        'title': 'Python for Data Science',
+        'description': 'Learn Python in a data science context.',
+        'url': 'https://example.com/python-data-science',
+        'duration': '15:30',
+    },
+]
