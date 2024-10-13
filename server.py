57678b3b0afa40257786338db718b1ec8989f744
Abhinand <wabhinandj@gmail.com>
2024-10-12 19:39:17 -0700

    imported retrieve_videos and redefined songs[{}]

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ from flask import request
 from flask import abort
 from flask_cors import CORS, cross_origin
 import os
+from retrieve_videos import get_video
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -18,25 +19,6 @@ for file in os.listdir(directory_path):
     if file.endswith(".txt"):
         songs.append(get_video(os.path.join(directory_path, file)))
 
-songs = [{
-        "image": "https://ged.com/wp-content/uploads/resized/2023/10/Online-GED-Test-Illustration_Copy_2-3x-768x0-c-default.png",
-        "name": "Into the Unknown",
-        "duration": "3:30",
-        "artist": "Idina Menzel",
-        "ytid": "gIOyB9ZXn8s",
-    },{
-        "image": "https://ged.com/wp-content/uploads/resized/2023/10/Online-GED-Test-Illustration_Copy_2-3x-768x0-c-default.png",
-        "name": "Love Story",
-        "duration": "3:56",
-        "artist": "Taylor Swift",
-        "ytid": "8xg3vE8Ie_E",
-    },{
-        "image": "https://ged.com/wp-content/uploads/resized/2023/10/Online-GED-Test-Illustration_Copy_2-3x-768x0-c-default.png",
-        "name": "song 1",
-        "duration": "3:45",
-        "artist": "bald nerd",
-        "ytid": "ZqY3eONjX5f",
-    }]
 
 @app.route("/")
 @cross_origin()
